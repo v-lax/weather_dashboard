1de18b912da59cd98c69d9e554e89d85c9d700b8
valliappan Lakshmanan <valliappanlakshmanan@valliappans-MacBook-Air.local>
2020-11-13 20:21:05 -0500

    updated javascript code to display uv index and five day forecast and also set up the giphy api

--- javascript/index.js
+++ javascript/index.js
@@ -28,6 +28,8 @@ function setupStorage(){
     localStorage.setItem("cities", JSON.stringify(city_array));
 }
 
+
+
 function displayWeatherOnSubmit(){
     var city = $('#city-input').val()
      var url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=" + APIKey + "&units=imperial";
@@ -45,12 +47,43 @@ function displayWeatherOnSubmit(){
          $('#humidity').text("Humidity: "+response.main.humidity+'%')
          $('#wind-speed').text("Wind-Speed: "+response.wind.speed+'MPH')
 
-         var giphyUrl = "http://api.giphy.com/v1/gifs/search?q="+response.weather[0].description+"&api_key="+giphyApiKey+"&limit=1"
+         var uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat="+response.coord.lat+"&lon="+response.coord.lon+"&appid="+APIKey
+
+         $.ajax({url:uvUrl,method:'GET'})
+          .then(function(uvResponse){
+            console.log(uvResponse)
+            var uvDiv=$('#uv-index')
+            uvDiv.text('UV Index: '+uvResponse.value)
+
+            if(uvResponse.value<=3){
+               uvDiv.attr('class','alert alert-dismissible alert-success')
+            }else if(uvResponse.value>=4 && uvResponse.value<7){
+               uvDiv.attr('class','alert alert-dismissible alert-primary')
+            }else if (uvResponse.value>=7){
+               uvDiv.attr('class','alert alert-dismissible alert-danger')
+            }
+
+          })
+
+          var fiveDayUrl = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+APIKey+ "&units=imperial"
+          $.ajax({url:fiveDayUrl,method:'GET'})
+           .then(function(fiveDayResponse){
+               console.log(fiveDayResponse)
+               for(var i=3;i<fiveDayResponse.list.length;i+=8){
+                   var date = fiveDayResponse.list[i].dt_txt.replace('12:00:00','')
+                   $(`#date${i}`).text(date)
+                   $(`#img${i}`).attr('src',"http://openweathermap.org/img/wn/"+fiveDayResponse.list[i].weather[0].icon+"@2x.png")
+                   $(`#temp${i}`).text("Temprature: "+fiveDayResponse.list[i].main.temp +"°F")
+                   $(`#hum${i}`).text("Humidity: "+fiveDayResponse.list[i].main.humidity+"%")
+
+               }
+           })
+
+         var giphyUrl = "http://api.giphy.com/v1/gifs/search?q="+response.weather[0].description+"&api_key="+giphyApiKey+"&limit=10&rating=g"
 
          $.ajax({url:giphyUrl,method:'GET'})
          .then(function(giphyResponse){
-             console.log(giphyResponse.data[0].embed_url)
-             $('#weather-giphy').attr('src',giphyResponse.data[0].embed_url)
+            $('#weather-giphy').attr('src',giphyResponse.data[Math.floor(Math.random()*10)].embed_url)
          })
      })
 }
@@ -61,6 +94,7 @@ function displayWeatherOnClick(){
 
     $.ajax({url:url,method:'GET'})
       .then(function(response){
+        console.log(response)
         var milliseconds = response.dt * 1000
         var dateObject = new Date(milliseconds)
         var humanDateFormat = dateObject.toLocaleString()
@@ -70,13 +104,42 @@ function displayWeatherOnClick(){
          $('#temprature').text("Temp: "+response.main.temp+"°F")
          $('#humidity').text("Humidity: "+response.main.humidity+'%')
          $('#wind-speed').text("Wind-Speed: "+response.wind.speed+'MPH')
+          
+         var uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat="+response.coord.lat+"&lon="+response.coord.lon+"&appid="+APIKey
 
-         var giphyUrl = "http://api.giphy.com/v1/gifs/search?q="+response.weather[0].description+"&api_key="+giphyApiKey+"&limit=1"
+          $.ajax({url:uvUrl,method:'GET'})
+           .then(function(uvResponse){
+             console.log(uvResponse)
+             var uvDiv=$('#uv-index')
+             uvDiv.text('UV Index: '+uvResponse.value)
+
+             if(uvResponse.value<=3){
+                uvDiv.attr('class','alert alert-dismissible alert-success')
+             }else if(uvResponse.value>=4 && uvResponse.value<7){
+                uvDiv.attr('class','alert alert-dismissible alert-primary')
+             }else if (uvResponse.value>=7){
+                uvDiv.attr('class','alert alert-dismissible alert-danger')
+             } 
+            })
+        var fiveDayUrl = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+APIKey+ "&units=imperial"
+            $.ajax({url:fiveDayUrl,method:'GET'})
+             .then(function(fiveDayResponse){
+                 console.log(fiveDayResponse)
+                 for(var i=3;i<fiveDayResponse.list.length;i+=8){
+                     var date = fiveDayResponse.list[i].dt_txt.replace('12:00:00','')
+                     $(`#date${i}`).text(date)
+                     $(`#img${i}`).attr('src',"http://openweathermap.org/img/wn/"+fiveDayResponse.list[i].weather[0].icon+"@2x.png")
+                     $(`#temp${i}`).text("Temprature: "+fiveDayResponse.list[i].main.temp +"°F")
+                     $(`#hum${i}`).text("Humidity: "+fiveDayResponse.list[i].main.humidity+"%")
+
+                 }
+             })
+
+         var giphyUrl = "http://api.giphy.com/v1/gifs/search?q="+response.weather[0].description+"&api_key="+giphyApiKey+"&limit=10&rating=g"
 
          $.ajax({url:giphyUrl,method:'GET'})
          .then(function(giphyResponse){
-             console.log(giphyResponse.data[0].embed_url)
-             $('#weather-giphy').attr('src',giphyResponse.data[0].embed_url)
+             $('#weather-giphy').attr('src',giphyResponse.data[Math.floor(Math.random()*10)].embed_url)
          })
      })
 }
